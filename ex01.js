d3cf5f5fc155818834e43259d79cfdb88c0c8003
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-19 11:58:15 -0300

    desafio 01

---
+++ ex01.js
@@ -0,0 +1,20 @@
+/*calcular valor de uma viagem
+
+3 variareis
+1- preço combustivel
+2- gasto medio de combustivel
+3- distancia em KM
+
+*/
+
+
+let comb = 3.50
+//preço combustivel
+let kmG = 10
+//gasto medio por viagem
+let dis = 130
+//distancia do percurso
+
+let lpd = dis / kmG
+let valor = lpd * comb
+console.log(valor)
