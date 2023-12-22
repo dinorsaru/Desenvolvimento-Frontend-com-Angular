77e48c785a732e12a2b33bae84b34289ffdc5b90
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-22 15:15:32 -0300

    mais objetos e funçoes

--- 04-objetos/aula06.js
+++ 04-objetos/aula06.js
@@ -4,22 +4,25 @@ class pessoa{
     nome
     idade
     
+    // como criar um default
+    constructor(nome, idade){
+        this.nome = nome
+        this.idade = idade
+    }
+
     descrever(){
         console.log(`meu nome é ${this.nome} e minha idade é ${this.idade} anos de idade`)
     }
 }
 
-let vitor = new pessoa()
-vitor.nome = 'vitor vitor'
-vitor.idade = 25
+/*let vitor = new pessoa('vitor', 25)
 
-let fui = new pessoa()
-fui.nome = ' fui fui'
-fui.idade= 10
+let fui = new pessoa('gui', 26)
 
-vitor.descrever()
+console.log(vitor)*/
 
-/*
+/* 
+
 const dados = {
     nome: 'gui',
     idade: 26,
@@ -28,3 +31,18 @@ const dados = {
         console.log(`meu nome é ${nome} e minha idade é ${idade} de idade`)
     }
 }*/
+
+function compararPessoas(p1, p2){
+    if (p1.idade > p2.idade){
+        console.log(`${p1.nome}é mais velha que ${p2.nome}`)
+    } else if ( p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velhor que ${p1.nome}`)
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} possuem a mesma idade`)
+    }
+}
+
+const vitor = new pessoa('vitor', 25)
+const gui = new pessoa('gui', 26)
+
+compararPessoas(vitor, gui)
