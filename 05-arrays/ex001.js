ce094a1c0a7d83470f371b6f20bead809d6b0ca6
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-23 23:51:33 -0300

    for na moralzinha

---
+++ 05-arrays/ex001.js
@@ -0,0 +1,8 @@
+/*programa tabuada */
+
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * 5)
+    
+}
---
+++ 05-arrays/ex002.js
@@ -0,0 +1,12 @@
+
+
+const num = [1, 3 , 56, 12 , 23, 24, 78, 14, 15]
+
+for (let i = 0; i < num.length; i++) {
+    if (i % 2 == 0){
+        console.log(num[i], 'par')
+    } else {
+        console.log(num[i], 'impar')
+    }
+    
+}
